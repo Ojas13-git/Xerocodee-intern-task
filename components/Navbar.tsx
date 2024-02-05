58ca7234998e048c240d4582df0e654d5fdb360a
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2024-02-05 19:18:17 +0530

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+// import Button from "./Button"
 
 const Navbar = () => {
   return (
-    <nav>
-        
-    </nav>
-  )
-}
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/logo.png" alt="logo" width={45} height={47} />
+      </Link>
 
-export default Navbar
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:text-blue-500 hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div className="lg:flexCenter hidden">
+        <span className="px-5">EN ˅</span>
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Schedule a Call
+        </button>
+      </div>
+
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
